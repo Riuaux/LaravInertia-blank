d589e764c2c11d04024113a8098b619e6c7dbee7
jonathan <jonathancggs@gmail.com>
2021-02-15 22:26:47 -0600

    version
    add scompact for versions more compact

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <p>@version('compact')</p>
+    <!--{{ env('APP_NAME') }}&nbsp;&nbsp;@version('scompact')-->
+    {{ env('APP_NAME') }}&nbsp;&nbsp;@version('compact')
     </div>
     <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
     reserved.
